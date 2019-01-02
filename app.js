291d0cb7a4fec3d95ddd78f072c0803ccdf63aa3
Felix Siebeneicker <felix.sieben@t-online.de>
2019-01-02 23:57:12 +0100

    Fixing !add command not working

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ let client = new tmi.Client({
 });
 client.connect();
 
-let handleAddCommand = function(channel, userstate, message) {
+let handleAddCommand = function (channel, userstate, message) {
     let commandName = message.split(/\s/)[1];
     if (commandName === undefined) {
         client.say(channel, "Usage: !add name message");
@@ -43,7 +43,7 @@ let handleAddCommand = function(channel, userstate, message) {
 
     commandName = commandName.toLowerCase();
 
-    if (["hello", "add", "remove", "commands", "disconnect", "info"].includes(commandName)) {
+    if (["hello", "add", "remove", "commands", "disconnect"].includes(commandName)) {
         client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
         return;
     }
@@ -56,7 +56,7 @@ let handleAddCommand = function(channel, userstate, message) {
 
     commandMessage = commandMessage.trim();
 
-    if (CommandManager.getCommand("!" + commandName)) {
+    if (CommandManager.getCommand("!" + commandName)[0]) {
         client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
         return;
     }
@@ -65,7 +65,7 @@ let handleAddCommand = function(channel, userstate, message) {
     client.say(channel, "@" + userstate["display-name"] + " Your command has been added and can now be used!");
 };
 
-let handleRemoveCommand = function(channel, userstate, message) {
+let handleRemoveCommand = function (channel, userstate, message) {
     let commandNameToRemove = message.split(/\s/)[1];
     if (commandNameToRemove === undefined) {
         client.say(channel, "Usage: !remove name");
@@ -100,8 +100,8 @@ let handleRemoveCommand = function(channel, userstate, message) {
     client.say(channel, "@" + userstate["display-name"] + " Your command has been removed!");
 };
 
-let handleCommandsCommand = function(channel) {
-    let commandNames = ["!hello", "!add", "!remove", "!commands", "!disconnect", "!info"];
+let handleCommandsCommand = function (channel) {
+    let commandNames = ["!hello", "!add", "!remove", "!commands", "!disconnect"];
     CommandManager.getCommands().forEach(command => {
         if (command.active && command.public) {
             commandNames.push(command.name);
@@ -110,31 +110,31 @@ let handleCommandsCommand = function(channel) {
     client.say(channel, "Available commands: " + commandNames.sort().join(", "));
 };
 
-let handleDisconnectCommand = function(channel, userstate) {
+let handleDisconnectCommand = function (channel, userstate) {
     if ("#" + userstate["username"] === channel) {
         CommandManager.saveCommands();
         client.disconnect();
     }
 };
 
-let handleHelloCommand = function(channel, userstate) {
+let handleHelloCommand = function (channel, userstate) {
     let greeting = config.greetings[Math.floor(Math.random() * config.greetings.length)];
     greeting = greeting.replace(/\{name\}/, userstate["display-name"]);
     client.say(channel, greeting);
 };
 
-let handleInfoCommand = function(channel) {
+/*let handleInfoCommand = function (channel) {
     let infos = CommandManager.getCommands(false);
     let info = infos[Math.floor(Math.random() * infos.length)];
     client.say(channel, info.message);
-};
+};*/
 
-client.on("chat", function(channel, userstate, message, self) {
+client.on("chat", function (channel, userstate, message, self) {
     if (self) { return; };
     if (!message.startsWith("!")) { return; };
 
     let commandName = message.split(/\s/)[0].toLowerCase();
-    switch(commandName) {
+    switch (commandName) {
         case "!disconnect":
             handleDisconnectCommand(channel, userstate);
             break;
@@ -155,12 +155,12 @@ client.on("chat", function(channel, userstate, message, self) {
             handleHelloCommand(channel, userstate);
             break;
 
-        case "!info":
-            handleInfoCommand(channel)
-            break;
+        //case "!info":
+        //    handleInfoCommand(channel)
+        //    break;
 
         default:
-            let [command, ] = CommandManager.getCommand(commandName);
+            let [command,] = CommandManager.getCommand(commandName);
             if (!command) {
                 return;
             }
