5014b81c4e3184dd13d8fcd67726bc8c9a47da03
Felix Siebeneicker <felix.sieben@t-online.de>
2019-01-19 16:55:48 +0100

    Refactoring code to be more functional instead of oo

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
 - Users in chat can create custom commands
 
 ## Installation
-1. Create configuration file `app.cfg.json` based on `app.cfg.example.json` contents.
+1. Create configuration file `app.cfg.json` based on `app.cfg.example.json` contents and **adjust for your needs**.
 1. Create command database file `commands-base.json` based on `commands.example.json` contents for your default commands.
 1. Create command database file `commands-[stream].json` based on `commands.example.json` contents for your individual `[stream]` commands. User commands will be saved here. E.g. `commands-dark-souls-3.json`.
+1. Create hashtag file `hashtag-[stream].json` for your individual `[stream]` hashtag. User hashtags will be saved here. E.g. `hashtag-dark-souls-3.json`.
 1. Install dependencies using `$ npm install`
 1. Run Chat Bot using `$ node app.js`
 1. Stop Chat Bot by writing `!disconnect` in Twitch Chat
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
-const path = require("path");
 const tmi = require("tmi.js");
 const logger = require("tmi.js/lib/logger");
-
 const config = require("./app.cfg.json");
-let CommandManager = require("./twitch-chat-bot-js/command-manager");
 
-let currentFilePath = path.dirname(fs.realpathSync(__filename));
+let commandsBasePath = config.commandsBasePath;
+let commandsStreamPath = config.commandsStreamPath;
+let hashtagPath = config.hashtagStreamPath;
 
-CommandManager.init(
-    path.join(currentFilePath, config.baseCommands),
-    path.join(currentFilePath, config.streamCommands)
-);
+let commandsBase = [];
+let commandsStream = [];
+
+let builtInCommandNames = ["!add", "!remove", "!commands", "!disconnect", "!hashtag"];
+
+let timeBetweenHashtagChanges = config.timeBetweenHashtagChanges;
+let timeOfLastHashtagChange = Date.now();
 
 let client = new tmi.Client({
     options: {
@@ -26,162 +28,325 @@ let client = new tmi.Client({
     },
     channels: [config.channel]
 });
-client.connect();
 
-let handleAddCommand = function (channel, userstate, message) {
-    let commandName = message.split(/\s/)[1];
-    if (commandName === undefined) {
-        client.say(channel, "Usage: !add name message");
-        return;
-    }
-
-    let messageIndex = "!add".length + commandName.length + 2;
-
-    if (commandName.startsWith("!")) {
-        commandName = commandName.substr(1);
-    }
-
-    commandName = commandName.toLowerCase();
-
-    if (["hello", "add", "remove", "commands", "disconnect"].includes(commandName)) {
-        client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
-        return;
-    }
-
-    let commandMessage = message.substr(messageIndex);
-    if (!commandMessage) {
-        client.say(channel, "Usage: !add name message");
-        return;
-    }
-
-    commandMessage = commandMessage.trim();
-
-    if (CommandManager.getCommand("!" + commandName)[0]) {
-        client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
-        return;
-    }
-
-    CommandManager.addCommand(commandName, commandMessage, userstate["username"]);
-    client.say(channel, "@" + userstate["display-name"] + " Your command has been added and can now be used!");
+let initCommands = function () {
+    commandsBase = require(commandsBasePath);
+    commandsStream = require(commandsStreamPath);
 };
 
-let handleRemoveCommand = function (channel, userstate, message) {
-    let commandNameToRemove = message.split(/\s/)[1];
-    if (commandNameToRemove === undefined) {
-        client.say(channel, "Usage: !remove name");
-        return;
+let saveCommands = function () {
+    fs.writeFile(
+        commandsStreamPath,
+        JSON.stringify(commandsStream, null, 4) + "\n",
+        (error) => {
+            if (error) { logger.info("Error writing commandsStream file: " + error); };
+        }
+    );
+};
+
+let getCommand = function (name) {
+    for (let i = 0; i < commandsBase.length; i++) {
+        if (commandsBase[i].name === name) {
+            return commandsBase[i];
+        }
     }
 
-    if (commandNameToRemove.startsWith("!")) {
-        commandNameToRemove = commandNameToRemove.substr(1);
+    for (let i = 0; i < commandsStream.length; i++) {
+        if (commandsStream[i].name === name) {
+            return commandsStream[i];
+        }
     }
 
-    commandNameToRemove = commandNameToRemove.toLowerCase();
-    let [commandToRemove, commandToRemoveType] = CommandManager.getCommand("!" + commandNameToRemove);
-    if (!commandToRemove) {
-        if (["hello", "add", "remove", "commands", "disconnect"].includes(commandNameToRemove)) {
-            client.say(channel, "@" + userstate["display-name"] + " This command cannot be removed!");
+    return undefined;
+};
+
+let getCommands = function (active = true) {
+    commands = [];
+    commands = commands.concat(commandsBase);
+    commands = commands.concat(commandsStream);
+    return commands.filter(command => command.active === active);
+};
+
+let addCommand = function (name, message, author, authorDisplay) {
+    commandsStream.push({
+        name: name,
+        message: message,
+        author: author,
+        authorDisplay: authorDisplay,
+        active: true
+    });
+};
+
+let removeCommand = function (name) {
+    for (let i = 0; i < commandsStream.length; i++) {
+        if (commandsStream[i].name === name) {
+            commandsStream.splice(i, 1);
             return;
         }
-
-        client.say(channel, "@" + userstate["display-name"] + " This command does not exist! Try out !commands to see a list of all available commands!");
-        return;
     }
-    if (commandToRemoveType === "base") {
-        client.say(channel, "@" + userstate["display-name"] + " This command cannot be removed!");
-        return;
-    }
-    if (commandToRemove.author !== userstate["username"]) {
-        client.say(channel, "@" + userstate["display-name"] + " The command can only be removed by @" + commandToRemove.author);
-        return;
-    }
-
-    CommandManager.removeCommand(commandToRemove.name);
-    client.say(channel, "@" + userstate["display-name"] + " Your command has been removed!");
 };
 
-let handleCommandsCommand = function (channel) {
-    let commandNames = ["!hello", "!add", "!remove", "!commands", "!disconnect"];
-    CommandManager.getCommands().forEach(command => {
-        if (command.active && command.public) {
-            commandNames.push(command.name);
-        }
-    });
-    client.say(channel, "Available commands: " + commandNames.sort().join(", "));
-};
-
-let handleDisconnectCommand = function (channel, userstate) {
+let handleCommandDisconnect = function (userstate, channel) {
     if ("#" + userstate["username"] === channel) {
-        CommandManager.saveCommands();
+        logger.info("Disconnecting from " + channel);
+        saveCommands();
         client.disconnect();
     }
 };
 
-let handleHelloCommand = function (channel, userstate) {
-    let greeting = config.greetings[Math.floor(Math.random() * config.greetings.length)];
-    greeting = greeting.replace(/\{name\}/, userstate["display-name"]);
-    client.say(channel, greeting);
+let handleCommandAdd = function (userstate, commandMessage, channel) {
+    let commandName = commandMessage.split(/\s/)[0];
+    let commandNameLength = commandName.length;
+    if (!commandName) {
+        if (config.verbose) {
+            client.say(channel, "Usage: !add name message");
+        }
+        else {
+            logger.info("Usage: !add name message");
+        }
+        return;
+    }
+
+    if (!commandName.startsWith("!")) {
+        commandName = "!" + commandName;
+    }
+
+    commandName = commandName.toLowerCase();
+
+    if (builtInCommandNames.includes(commandName)) {
+        if (config.verbose) {
+            client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
+        }
+        else {
+            logger.info("@" + userstate["display-name"] + " This command already exists!");
+        }
+        return;
+    }
+
+    let message = commandMessage.substr(commandNameLength);
+    if (!message) {
+        if (config.verbose) {
+            client.say(channel, "Usage: !add name message");
+        }
+        else {
+            logger.info("Usage: !add name message");
+        }
+        return;
+    }
+
+    message = message.trim();
+
+    if (getCommand(commandName)) {
+        if (config.verbose) {
+            client.say(channel, "@" + userstate["display-name"] + " This command already exists!");
+        }
+        else {
+            logger.info("@" + userstate["display-name"] + " This command already exists!");
+        }
+        return;
+    }
+
+    addCommand(commandName, message, userstate["username"], userstate["display-name"]);
+    if (config.verbose) {
+        client.say(channel, "@" + userstate["display-name"] + " Your command has been added and can now be used!");
+    }
+    else {
+        logger.info("@" + userstate["display-name"] + " Your command has been added and can now be used!");
+    }
 };
 
-/*let handleInfoCommand = function (channel) {
-    let infos = CommandManager.getCommands(false);
-    let info = infos[Math.floor(Math.random() * infos.length)];
-    client.say(channel, info.message);
-};*/
+let handleCommandRemove = function (userstate, commandMessage, channel) {
+    let commandNameToRemove = commandMessage.split(/\s/)[0];
+    if (!commandNameToRemove) {
+        if (config.verbose) {
+            client.say(channel, "Usage: !remove name");
+        }
+        else {
+            logger.info("Usage: !remove name");
+        }
+        return;
+    }
+
+    if (!commandNameToRemove.startsWith("!")) {
+        commandNameToRemove = "!" + commandNameToRemove;
+    }
+
+    commandNameToRemove = commandNameToRemove.toLowerCase();
+
+    let commandToRemove = getCommand(commandNameToRemove);
+    if (!commandToRemove) {
+        if (builtInCommandNames.includes(commandNameToRemove)) {
+            if (config.verbose) {
+                client.say(channel, "@" + userstate["display-name"] + " This command cannot be removed!");
+            }
+            else {
+                logger.info("@" + userstate["display-name"] + " This command cannot be removed!");
+            }
+        }
+        else {
+            if (config.verbose) {
+                client.say(channel, "@" + userstate["display-name"] + " This command does not exist! Try out !commands to see a list of all available commands!");
+            }
+            else {
+                logger.info("@" + userstate["display-name"] + " This command does not exist! Try out !commands to see a list of all available commands!");
+            }
+        }
+        return;
+    }
+
+    if (commandToRemove.author !== userstate["username"]) {
+        if (config.verbose) {
+            client.say(channel, "@" + userstate["display-name"] + " This command can only be removed by @" + commandToRemove.authorDisplay + " !");
+        }
+        else {
+            logger.info("@" + userstate["display-name"] + " This command can only be removed by @" + commandToRemove.authorDisplay + " !");
+        }
+        return;
+    }
+
+    removeCommand(commandToRemove.name);
+    if (config.verbose) {
+        client.say(channel, "@" + userstate["display-name"] + " Your command has been removed!");
+    }
+    else {
+        logger.info("@" + userstate["display-name"] + " Your command has been removed!");
+    }
+};
+
+let handleCommandCommands = function (channel) {
+    let commandNames = builtInCommandNames.slice();
+    getCommands().forEach(command => {
+        if (command.active) {
+            commandNames.push(command.name);
+        }
+    });
+    if (config.verbose) {
+        client.say(channel, "Available commands: " + commandNames.sort().join(", "));
+    }
+    else {
+        logger.info("Available commands: " + commandNames.sort().join(", "));
+    }
+};
+
+let handleCommandHashtag = function (userstate, commandMessage, channel) {
+    commandMessage = commandMessage.trim();
+    if (!commandMessage) {
+        if (config.verbose) {
+            client.say(channel, "Usage: !hashtag message");
+        }
+        else {
+            logger.info("Usage: !hashtag message");
+        }
+        return;
+    }
+
+    if (!commandMessage.startsWith("#")) {
+        commandMessage = "#" + commandMessage;
+    }
+
+    commandMessage = commandMessage.split(/\s/).map(function(element) {
+        if (element) {
+            return element[0].toUpperCase() + element.slice(1);
+        }
+    }).join("");
+
+    let timeDelta = Date.now() - timeOfLastHashtagChange;
+
+    if (timeDelta < timeBetweenHashtagChanges * 1000) {
+        if (config.verbose) {
+            client.say(channel, "@" + userstate["display-name"] + " The hashtag can be changed again in " + (timeBetweenHashtagChanges - Math.floor(timeDelta / 1000)) + " seconds!");
+        }
+        else {
+            logger.info("@" + userstate["display-name"] + " The hashtag can be changed again in " + (timeBetweenHashtagChanges - Math.floor(timeDelta / 1000)) + " seconds!");
+        }
+        return;
+    }
+
+    timeOfLastHashtagChange = Date.now();
+
+    fs.writeFile(
+        hashtagPath,
+        commandMessage,
+        (error) => {
+            if (error) { logger.info("Error writing hashtag fille: " + error); }
+        }
+    );
+
+    if (config.verbose) {
+        client.say(channel, "@" + userstate["display-name"] + " Your hashtag has been set!");
+    }
+    else {
+        logger.info("@" + userstate["display-name"] + " Your hashtag has been set!");
+    }
+};
+
+let handleCommand = function (commandName, channel) {
+    let command = getCommand(commandName);
+    if (!command) { return; }
+
+    if ("#" + command.author === channel) {
+        if (config.verbose) {
+            client.say(channel, command.message);
+        }
+        else {
+            logger.info(command.message);
+        }
+    }
+    else {
+        if (config.verbose) {
+            client.say(channel, command.message + " (" + command.authorDisplay + ")");
+        }
+        else {
+            logger.info(command.message + " (" + command.authorDisplay + ")");
+        }
+    }
+};
+
+client.on("join", function (channel, username, self) {
+    if (!self) {
+        logger.info("[" + channel + "] " + username + " joined");
+    }
+});
+
+client.on("part", function (channel, username, self) {
+    if (!self) {
+        logger.info("[" + channel + "] " + username + " left");
+    }
+});
 
 client.on("chat", function (channel, userstate, message, self) {
     if (self) { return; };
     if (!message.startsWith("!")) { return; };
 
     let commandName = message.split(/\s/)[0].toLowerCase();
+    let commandMessage = message.slice(commandName.length).trim();
+
     switch (commandName) {
-        case "!disconnect":
-            handleDisconnectCommand(channel, userstate);
+        case ("!disconnect"):
+            handleCommandDisconnect(userstate, channel);
             break;
 
-        case "!add":
-            handleAddCommand(channel, userstate, message);
+        case ("!add"):
+            handleCommandAdd(userstate, commandMessage, channel);
             break;
 
-        case "!remove":
-            handleRemoveCommand(channel, userstate, message);
+        case ("!remove"):
+            handleCommandRemove(userstate, commandMessage, channel);
             break;
 
-        case "!commands":
-            handleCommandsCommand(channel);
+        case ("!commands"):
+            handleCommandCommands(channel);
             break;
 
-        case "!hello":
-            handleHelloCommand(channel, userstate);
+        case ("!hashtag"):
+            handleCommandHashtag(userstate, commandMessage, channel);
             break;
-
-        //case "!info":
-        //    handleInfoCommand(channel)
-        //    break;
 
         default:
-            let [command,] = CommandManager.getCommand(commandName);
-            if (!command) {
-                return;
-            }
-            if ("#" + command.author === channel) {
-                client.say(channel, command.message);
-            }
-            else {
-                client.say(channel, command.message + " (" + command.author + ")");
-            }
+            handleCommand(commandName, channel);
             break;
     }
 });
 
-client.on("join", function (channel, username, self) {
-    if (!self) {
-        logger.info(username + " joined " + channel);
-    }
-});
-
-client.on("part", function (channel, username, self) {
-    if (!self) {
-        logger.info(username + " left " + channel);
-    }
-});
+initCommands();
+client.connect();
